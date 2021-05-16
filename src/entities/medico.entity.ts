253545dbfc3d10a66070c804e049cb48c8854f99
Fernando Acosta <Fernando Acosta>
2021-05-16 14:26:46 -0500

    create medico entity

---
+++ src/entities/medico.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+import { Persona } from './persona'
+import { Usuario } from './usuario.entity'
+
+@Entity()
+export class Medico extends Persona {
+  @PrimaryGeneratedColumn('uuid')
+  medico_id: string
+
+  @Column({ length: 13 })
+  rfc: string
+
+  @Column({ length: 8 })
+  cedula: string
+
+  @ManyToOne(() => Usuario, {
+    cascade: true,
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'usuario_id' })
+  usuario: Usuario
+}
