4bd6967bf2757fd2379a28bffeae4fbd32d9a795
Fernando Acosta <Fernando Acosta>
2021-06-03 00:41:38 -0500

    auth medicamentos routes

--- src/modules/medicamento/medicamento.routes.ts
+++ src/modules/medicamento/medicamento.routes.ts
@@ -2,8 +2,12 @@ import { Router } from 'express'
 import * as Controller from './medicamento.controller'
 import { check } from 'express-validator'
 import { validateBody } from '../../middlewares/validate_body'
+import passport from '../../libs/passport'
 
 const router = Router()
+
+// ---------------------- Auth -------------------------------------
+router.use(passport.authenticate('jwt', { session: false }))
 
 router.get('/', Controller.findAll)
 router.get('/:medicamento_id', Controller.findOne)
