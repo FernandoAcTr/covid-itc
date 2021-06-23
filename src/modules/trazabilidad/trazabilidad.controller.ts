78d3a38daa16cf4e7d6f59e8315c3b664c7a3323
wugi <17031278@itcelaya.edu.mx>
2021-06-23 18:53:09 -0500

    added email functionality

--- src/modules/trazabilidad/trazabilidad.controller.ts
+++ src/modules/trazabilidad/trazabilidad.controller.ts
@@ -1,6 +1,11 @@
+import dateformat from 'dateformat'
 import { NextFunction, Request, Response } from 'express'
 import { getCustomRepository } from 'typeorm'
 import { TrazabilidadRepository } from './trazabilidad.repository'
+import { compile } from '../../helpers/compile_hbs'
+import { sendMail } from '../../libs/mailer'
+import { settings } from '../../config/settings'
+const { EMAIL_CREDENTIALS } = settings
 
 export async function getTrazability(
   req: Request,
@@ -30,15 +35,24 @@ export async function findAll(
   }
 }
 
-export async function addContact(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> {
+export async function addContact(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const trazabilidad = await getCustomRepository(
       TrazabilidadRepository
     ).addContact(req.body)
+
+    const email = compile('alerta.hbs', {
+      fecha: dateformat(Date.now(), 'dd-mm-yyyy'),
+      alerta: "Una persona con la que ha estado en contacto se encuentra contagiada de COVID-19, le recomendamos ir al medico a realizarse una prueba.",
+      departamento: 'Departamento Médico',
+    })
+    sendMail(
+      EMAIL_CREDENTIALS.EMAIL!,
+      trazabilidad.contacto.email,
+      'Ha recibido una nueva alerta',
+      email
+    )
+
     res.json(trazabilidad)
   } catch (error) {
     next(error)
